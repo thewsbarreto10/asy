e8ae9185c3e54684be0a6fdc5539d0938f436304
thewsbarreto10 <mathews.sbarreto@gmail.com>
2026-04-13 15:02:18 -0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -3,9 +3,9 @@
 $env = parse_ini_file(__DIR__ . '/config.env');
 
 return [
-    'host'    => $env['DB_HOST'] ?? 'localhost',
+    'host'    => $env['DB_HOST'] ?? '',
     'db'      => $env['DB_NAME'] ?? '',
     'user'    => $env['DB_USER'] ?? '',
     'pass'    => $env['DB_PASS'] ?? '',
-    'charset' => $env['DB_CHARSET'] ?? 'utf8mb4'
+    'charset' => $env['DB_CHARSET'] ?? ''
 ];
